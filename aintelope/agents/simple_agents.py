be86f2ffe46bbce21674604bc23e4a86c5f75084
Nathan Helm-Burger <nathan.helm.burger@gmail.com>
2022-09-20 13:46:46 -0700

    simple.py split into simple_eval.py and simple_agents.py

--- aintelope/agents/simple_agents.py
+++ aintelope/agents/simple_agents.py
@@ -1,19 +1,10 @@
-import random
-from pprint import pprint
-import numpy as np
-from gym.spaces import Discrete
+import typing as typ
+
 import gym
-from aintelope.agents.q_agent import Agent as Q_agent
-from aintelope.agents.memory import ReplayBuffer
-from aintelope.aintelope.environments.savanna_zoo import env, move_agent, reward_agent
-import os
-from gemini import Scene, Entity, sleep
+import numpy as np
+import torch
+from torch import nn
 
-
-
-
-# pygame.display.list_modes()
-os.environ["SDL_VIDEODRIVER"] = "dummy" #"x11" # "directfb"
 
 # numerical constants
 EPS = 0.0001
@@ -118,45 +109,3 @@ class IterativeWeightOptimizationAgent:
         info[LAST_REWARD_KEY] = reward
         print("chose action", action)
         return action
-
-
-def main(env):
-    mode = "ascii"
-    # policy = IterativeWeightOptimizationAgent()
-    # policy = OneStepPerfectPredictionAgent()
-    # policy = RandomWalkAgent()
-    
-    replay_size = 1000
-    env.reset()
-    replay_buffer = ReplayBuffer(replay_size)
-    policy = Q_agent(gym.make(env), replay_buffer)
-    for agent in env.agent_iter():
-        observation, reward, done, info = env.last()
-
-        action_space: Discrete = env.action_space(agent)
-        if not done:
-            action = policy(action_space, observation, reward, info)
-            assert action in action_space
-        else:
-            action = None
-        env.step(action)
-        env.render(mode)
-
-    # wait = input("Close?")
-
-
-if __name__ == "__main__":
-    env_params = {
-        'NUM_ITERS':40,  # duration of the game
-        'MAP_MIN':0, 
-        'MAP_MAX':20,
-        'render_map_max':20,
-        'AMOUNT_AGENTS':1,  # for now only one agent
-        'AMOUNT_GRASS_PATCHES':2
-                }
-    e = env(env_params=env_params)
-    main(e)
-
-# Local Variables:
-# compile-command: "poetry run python simple.py"
-# End:
